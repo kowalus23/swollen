12069ffe99f118b46e8a4b9d21f9d2f635ef45ef
Kowalus23 <Kowalus23@gmail.com>
2025-05-31 18:50:56 +0200

    fix

--- src/components/Pages/home/Hero.tsx
+++ src/components/Pages/home/Hero.tsx
@@ -3,8 +3,8 @@ import styles from './Hero.module.scss';
 export default function Hero() {
   return (
     <div className={styles.container}>
+      <div className={styles.gridBackground} />
       <h1 className={styles.title}>HOME</h1>
-      <p className={styles.description}>This is the home page.</p>
     </div>
   );
 }
