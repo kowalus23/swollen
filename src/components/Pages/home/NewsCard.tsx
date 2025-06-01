997a0037d5448ea3246cc9d630ebe8bdcafa33b4
Kowalus23 <Kowalus23@gmail.com>
2025-06-01 09:00:05 +0200

    updt

--- src/components/Pages/home/NewsCard.tsx
+++ src/components/Pages/home/NewsCard.tsx
@@ -7,8 +7,8 @@ export default function NewsCard() {
         <span className={styles.title}>AKTUALIZACJA</span>
         <span className={styles.iconBox}>
           <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect x="4" y="4" width="32" height="32" rx="2" stroke="#FFD600" strokeWidth="2" fill="none" />
-            <text x="50%" y="60%" textAnchor="middle" fill="#FFD600" fontSize="28" fontWeight="bold" dy=".3em">!</text>
+            <rect x="4" y="4" width="32" height="32" rx="2" stroke="#F6BA00" strokeWidth="2" fill="none" />
+            <text x="50%" y="60%" textAnchor="middle" fill="#F6BA00" fontSize="28" fontWeight="bold" dy=".3em">!</text>
           </svg>
         </span>
       </div>
