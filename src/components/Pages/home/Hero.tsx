ff229b991abe1820364838ab4151d956cc038437
Kowalus23 <Kowalus23@gmail.com>
2025-06-12 10:51:24 +0200

    updt

--- src/components/Pages/home/Hero.tsx
+++ src/components/Pages/home/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className={styles.container}>
       <div className={styles.gridBackground} />
       <div className={styles.content}>
-        <Image src="/images/logo.svg" alt="Logo" width={555} height={281} style={{ marginBottom: 24 }} />
+        <Image className={styles.logo} src="/images/logo.svg" alt="Logo" width={555} height={281} style={{ marginBottom: 24 }} />
         <NewsCard />
       </div>
     </section>
