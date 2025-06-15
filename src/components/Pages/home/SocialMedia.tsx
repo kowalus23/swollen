1285254824b55d4f49cd11dd7a9c92f580d948d9
Kowalus23 <Kowalus23@gmail.com>
2025-06-15 11:57:29 +0200

    updt

--- src/components/Pages/home/SocialMedia.tsx
+++ src/components/Pages/home/SocialMedia.tsx
@@ -3,6 +3,7 @@ import styles from './SocialMedia.module.scss';
 export default function SocialMedia() {
   return (
     <section className={styles.socialMediaSection}>
+      <div className={styles.socialMediaGridBackground} />
       <h1 className={styles.title}>SOCIAL MEDIA</h1>
     </section>
   );
