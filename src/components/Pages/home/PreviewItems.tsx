8a4c13ff36bbb9513008330c8f8fb2f1f0df72f1
Kowalus23 <Kowalus23@gmail.com>
2025-06-17 07:37:41 +0200

    login

--- src/components/Pages/home/PreviewItems.tsx
+++ src/components/Pages/home/PreviewItems.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/Button';
 import { useNewCollection } from '@/hooks/useNewCollection';
+import { useUserStore } from '@/store/userStore';
 import Image from 'next/image';
 import styles from './PreviewItems.module.scss';
 
 export default function PreviewItems() {
   const { data: newCollection, isLoading } = useNewCollection();
+  const user = useUserStore((state) => state.user);
 
   if (isLoading || !newCollection) {
     return null;
@@ -15,15 +17,17 @@ export default function PreviewItems() {
       <div className={styles.previewItemsGridBackground} />
       <div className={styles.previewItemsBackground} />
       <div className={styles.previewItemsContainer}>
-        <div className={styles.previewItemsImages}>
-          <Image
-            src={newCollection.previewImages[0]}
-            alt={`New collection preview`}
-            width={500}
-            height={500}
-            className={styles.image}
-          />
-        </div>
+        {user?.email && (
+          <div className={styles.previewItemsImages}>
+            <Image
+              src={newCollection.previewImages[0]}
+              alt={`New collection preview`}
+              width={500}
+              height={500}
+              className={styles.image}
+            />
+          </div>
+        )}
         <div className={styles.previewItemsContent}>
           <h2 className={styles.title}>NOWA KOLEKCJA</h2>
           <p className={styles.description}>
@@ -36,15 +40,17 @@ export default function PreviewItems() {
             POWIADOM MNIE O PREMIERZE
           </Button>
         </div>
-        <div className={styles.previewItemsImages}>
-          <Image
-            src={newCollection.previewImages[1]}
-            alt={`New collection preview`}
-            width={500}
-            height={500}
-            className={styles.image}
-          />
-        </div>
+        {user?.email && (
+          <div className={styles.previewItemsImages}>
+            <Image
+              src={newCollection.previewImages[1]}
+              alt={`New collection preview`}
+              width={500}
+              height={500}
+              className={styles.image}
+            />
+          </div>
+        )}
 
         <div className={styles.decorationImageContainer}>
           <Image className={styles.decorationImage} src="/images/fine-hand-image.png" alt="Sun" width={66} height={58} />
